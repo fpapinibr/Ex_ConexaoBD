7295768b605207873d20c537667473f8847ec807
fpapinibr <fpapini@gmail.com>
2022-09-22 16:25:36 -0300

    Atualizado na Aula de Hoje 22/09/2022

--- Ex_ConexaoBD/Program.cs
+++ Ex_ConexaoBD/Program.cs
@@ -21,12 +21,13 @@ namespace Ex_ConexaoBD
         {
             try
             {
+                //Montando a String de Conexao
                 var datasource = @"DESKTOP-PJOKI67\SQLEXPRESS";//instancia do servidor
                 var database = "Faculdade_XPTO"; //Base de Dados
                 var username = "sa"; //usuario da conexão
                 var password = "123@abc"; //senha
 
-                //sua string de conexão 
+                //Concatenando as informações de sua string de conexão 
                 string connString = @"Data Source=" + datasource + ";Initial Catalog="
                             + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
 
@@ -36,14 +37,14 @@ namespace Ex_ConexaoBD
                 using (connection)
                 {
                     //Exemplo de Comando SQL - INSERT
-                    Comando_SQL_INSERCAO(connection);
+                    //Comando_SQL_INSERCAO(connection);
 
                     // Exemplo de Comando SQL
                     Comando_SQL(connection);
 
                     // Exemplo de Chamada de Stored Procedure
-                    Console.WriteLine("\n\nChamando a Procedure...");
-                    Call_SP(connection);
+                    //Console.WriteLine("\n\nChamando a Procedure NOTA2...");
+                   // Call_SP(connection);
                 }
             }
             catch (SqlException e)
@@ -61,7 +62,7 @@ namespace Ex_ConexaoBD
             connection.Open();
             SqlCommand command = new SqlCommand(null, connection);
 
-            // Cria e preapara a estrutura do Comando SQL.
+            // Cria e prepapara a estrutura do Comando SQL.
             command.CommandText =
                 "INSERT INTO Aluno (ra, nome) " +
                 "VALUES (@ra, @nome)";
@@ -77,31 +78,29 @@ namespace Ex_ConexaoBD
             // Executa primeiro a preparação do comando após fazer as configurações do mesmo.
             command.Prepare();
             command.ExecuteNonQuery();
-
             connection.Close();
         }
 
         public static void Call_SP(SqlConnection connection)
         {
-            //Definição dos parâmetros
+            // Definição dos parâmetros de forma fixa (Estática) -
+            // Poderia ter feito o usuário informar esses valores
             int ra = 33221100;
-            string disciplina = "33001";
+            string disciplina = "33002";
             int ano = 2022;
             int semestre = 1;
-            double  nota1 = 3.75;
-
+            double  nota1 = 5.5;
 
             using (connection)
             {
                 connection.Open();
-                SqlCommand sql_cmnd = new SqlCommand("Inserir_Nota1", connection);
+                SqlCommand sql_cmnd = new SqlCommand("Inserir_Nota", connection);
                 sql_cmnd.CommandType = CommandType.StoredProcedure;
                 sql_cmnd.Parameters.AddWithValue("@Aluno", SqlDbType.Int).Value = ra;
                 sql_cmnd.Parameters.AddWithValue("@Disciplina", SqlDbType.NVarChar).Value = disciplina;
                 sql_cmnd.Parameters.AddWithValue("@Ano", SqlDbType.Int).Value = ano;
                 sql_cmnd.Parameters.AddWithValue("@Semestre", SqlDbType.Int).Value = semestre;
-                sql_cmnd.Parameters.AddWithValue("@Nota1", SqlDbType.Float).Value = nota1;
-                //sql_cmnd.Parameters.AddWithValue("@AGE", SqlDbType.Int).Value = age;
+                sql_cmnd.Parameters.AddWithValue("@Nota2", SqlDbType.Float).Value = nota1;
                 sql_cmnd.ExecuteNonQuery();
                 connection.Close();
             }
